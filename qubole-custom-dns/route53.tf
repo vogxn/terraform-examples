a0a05b9db52ff7eecb2dbb8f6e9bdc3940bd62d2
Prasanna Santhanam <tsp@qubole.com>
2016-06-24 17:45:22 +0530

    only one dhcp option set per vpc :o

--- qubole-custom-dns/route53.tf
+++ qubole-custom-dns/route53.tf
@@ -8,21 +8,11 @@ resource "aws_route53_zone" "qubole-dns" {
 
 /* DHCP Options */
 resource "aws_vpc_dhcp_options" "qubole_custom_dhcp" {
-  domain_name = "qubole-dns.net"
+  domain_name = "qubole-dns.net ec2.internal"
   domain_name_servers = ["AmazonProvidedDNS"]
 }
 
 resource "aws_vpc_dhcp_options_association" "qubole_custom_dhcp_assoc" {
     vpc_id = "${aws_vpc.qubole-vpc-customdns.id}"
     dhcp_options_id = "${aws_vpc_dhcp_options.qubole_custom_dhcp.id}"
-}
-
-resource "aws_vpc_dhcp_options" "amzn_provided_dhcp" {
-  domain_name = "ec2.internal"
-  domain_name_servers = ["AmazonProvidedDNS"]
-}
-
-resource "aws_vpc_dhcp_options_association" "amzn_provided_dhcp_assoc" {
-    vpc_id = "${aws_vpc.qubole-vpc-customdns.id}"
-    dhcp_options_id = "${aws_vpc_dhcp_options.amzn_provided_dhcp.id}"
 }
